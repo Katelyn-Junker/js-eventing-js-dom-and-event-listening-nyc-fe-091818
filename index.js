3184f9116dfa7d4250c69acd2765d4aed41ce6b1
Katelyn Junker <kjunker@villanova.edu>
2018-10-30 22:32:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 const main= document.querySelector('#main')
 
-function onClick(){
+function onClick(event){
   alert('i was clicked')
 }
 
 main.addEventListener('click',onClick)
+
+const input= document.querySelector('input')
+
+function onKeyDown(event){
+  console.log(event)
+  if (event.which=== 74){
+    alert('what the j')
+  
+  }
+}
+
+input.addEventListener('keydown', )
