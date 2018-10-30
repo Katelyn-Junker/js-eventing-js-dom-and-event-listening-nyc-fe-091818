d246d7a43b4f5b3a7bf1fc11d94438ab38349f52
Katelyn Junker <kjunker@villanova.edu>
2018-10-30 22:44:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,4 +19,13 @@ function onKeyDown(event){
 input.addEventListener('keydown', onKeyDown)
 
 const divs= document.querySelectorAll('div')
+console.log(divs)
+
+function onDivClick(event){
+  console.log(event)
+}
+
+for(let div of divs){
+  div.addEventListener('click', )
+}
 
